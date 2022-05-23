3dbfaeb8b896ab1bf2b85ad896a61767539fc98a
Bruno Lisboa <brlisbo@gmail.com>
2022-05-23 17:14:31 -0300

    arreglado bug imagenes

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const pintarCards = data => {
     data.forEach(producto => {
         templateCard.querySelector('h5').textContent = producto.title
         templateCard.querySelector('p').textContent = producto.precio
-        templateCard.querySelector('img').setAttribute('img', producto.thumbnailUrl)
+        templateCard.querySelector('img').setAttribute('src', producto.thumbnailUrl)
         templateCard.querySelector('.btn-dark').dataset.id = producto.id
     
         const clone = templateCard.cloneNode(true)
